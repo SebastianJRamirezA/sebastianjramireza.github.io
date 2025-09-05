d168ce71ee028e0174e8f1ae1bc65b93d5733a23
Sebastian Ramirez <sebastian.jose.ramirez.a@gmail.com>
2025-09-05 19:08:29 -0400

    Add initial projects page implementation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Projects } from './projects/projects';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home,
+        title: 'Home Page',
+    },
+    {
+        path: 'projects',
+        component: Projects,
+        title: 'Projects',
+    }
+];
