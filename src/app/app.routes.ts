2f0dc7b281bc1e13897bb82db9db362785674056
Sebastian Ramirez <sebastian.jose.ramirez.a@gmail.com>
2025-09-05 19:22:05 -0400

    Add about me page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Projects } from './projects/projects';
+import { About } from './about/about';
 
 export const routes: Routes = [
     {
         path: '',
         component: Home,
         title: 'Home Page',
+    },
+    {
+        path: 'about',
+        component: About,
+        title: 'About Me',
     },
     {
         path: 'projects',
